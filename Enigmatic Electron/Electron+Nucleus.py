fe379f93bb879f95389ab35e80553f1452b06aaa
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2019-11-29 20:36:39 +0530

    Redefined some Animations.

--- Enigmatic Electron/Electron+Nucleus.py
+++ Enigmatic Electron/Electron+Nucleus.py
@@ -3,12 +3,12 @@ from manimlib.imports import *
 #are reset every frame. Therefore, all position and angle and similar variables that must remain unreset between updates must remain outside the function.
 # t_offset = 0
 initposEl=0
-class OrbitAnimation(Scene):
+class RutherfordsModel(Scene):
     def construct(self):
         
         nucleus= Dot(color = BLUE).center()
 
-        orbit = Circle(color=WHITE,stroke_width=1,center=Dot(color=BLUE))
+        orbit = Circle(color=WHITE,stroke_width=1,center=nucleus)
         
         electron = Dot(color=YELLOW)
 
@@ -17,9 +17,16 @@ class OrbitAnimation(Scene):
         def update_electron(electron, dt):
             global initposEl
             rate = dt
-            electron.move_to(orbit.point_from_proportion(((initposEl + rate))%1))
+            electron.move_to(orbit.point_from_proportion(((initposEl + rate))%1)) #point_from_proportion takes a value from -1,1 specifying direction to move in with sign and how much to move by with value
             initposEl += rate*2
 
         self.play(GrowFromCenter(H_atom))
         electron.add_updater(update_electron)
         self.wait(9)
+
+class ThomsonModel(Scene):
+    def construct(self):
+        shell=Sphere(stroke_color=WHITE,checkerboard_colors=[None,None])
+
+        self.play(GrowFromCenter(shell))
+        self.play(shell.)
