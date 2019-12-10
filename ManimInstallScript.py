5a0477409d56d9f057783106ee14edee80617848
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2019-12-10 14:15:48 +0530

    Started work on Manim Installer.

---
+++ ManimInstallScript.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Manim Installer")
+window.geometry("720x480")
+
+
+Heading = tk.Label(window, text = "Welcome to ManimInstaller!",font=(None,50)).pack()
+window.mainloop()
