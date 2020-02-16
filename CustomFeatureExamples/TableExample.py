1ab791b8225184a2d695502595636cfdb56c995c
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-02-16 22:03:39 +0530

    Updated TableExample.py

--- CustomFeatureExamples/TableExample.py
+++ CustomFeatureExamples/TableExample.py
@@ -6,13 +6,15 @@ class Tables(Scene):
         tabledict={
             TextMobject("TextMobject Input"):[TextMobject("Must"),TextMobject("add"),TextMobject("element"),TextMobject("retrieval.")],
             TexMobject("TexMobject Input"):[TexMobject(r"e^{\iota\pi}+1 = 0"),TexMobject(r"Tex: \alpha\theta\epsilon")],
-            Text("Text input",font="Lucida Grande"):[Text("Text",font="Alys Script Bold"),Text("is"),Text("Supported")],
+            Text("Text input",font="Lucida Grande"):[Text("Text",font="Alys Script Bold"),Text("is",font="serif"),Text("Supported",font="serif")],
             "Raw String Input":["Defaults","to","TextMobject."]
-
         }
 
-        table=Table.get_table(tabledict,line_color=GREY,raw_string_color=BLUE)
-        table.move_to((0,0,0))
-        self.play(Write(table.scale(0.5)),run_time=2)
+        table=Table(tabledict=tabledict,line_color=GRAY,raw_string_color=BLUE)
 
+        table.move_to((0,0,0))
+        table.scale(0.5)
+        self.play(Write(table),run_time=2)
+
+        table.add_record(TextMobject("Text").scale(0.5),1)
         self.wait(1)
