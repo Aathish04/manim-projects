afea2fe935a66e1f391eaff3366ab3fd33534081
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-02-22 09:49:02 +0530

    Updated TableExample to reflect Changes.

--- CustomFeatureExamples/TableExample.py
+++ CustomFeatureExamples/TableExample.py
@@ -18,9 +18,9 @@ class Tables(Scene):
 
         self.play(Write(table.add_record(record=TextMobject("New Value"),field_num=0)))
         
-        self.play(*table.adjust_lines())
+        self.play(table.adjust_lines())
 
         self.play(Uncreate(table.remove_record(field_num=1,record_num=0)))
         
-        self.play(*table.adjust_positions())
+        self.play(table.adjust_positions())
         self.wait(1)
