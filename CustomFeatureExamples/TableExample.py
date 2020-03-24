97dcb65f71ebbe32b3d9a6f9cdb5160bcbb984fd
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-03-24 22:37:05 +0530

    Updated TableExample to reflect changes in Table()

--- CustomFeatureExamples/TableExample.py
+++ CustomFeatureExamples/TableExample.py
@@ -8,6 +8,7 @@ class Tables(Scene):
             TexMobject("TexMobject Input"):[TexMobject(r"e^{\iota\pi}+1 = 0"),TexMobject(r"Tex: \alpha\theta\epsilon")],
             "Raw String Input":["Defaults","to","TextMobject."],
             Text("Text input",font="Lucida Grande"):[Text("Text",font="Alys Script Bold"),Text("is",font="serif"),Text("Supported",font="serif")],
+
         }
 
         table=Table(tabledict=tabledict,buff_length=0.3,line_color=GRAY,raw_string_color=BLUE)
@@ -16,7 +17,7 @@ class Tables(Scene):
         
         self.play(Write(table),run_time=2)
 
-        self.play(Write(table.add_record(record=TextMobject("New Value"),field_num=0)))
+        self.play(Write(table.add_record(record=Integer(123),field_num=0)))
         
         self.play(table.adjust_lines())
 
@@ -24,7 +25,7 @@ class Tables(Scene):
         
         self.play(table.adjust_positions())
         
-        self.play(Write(table.add_field(TextMobject("Hi"))))
+        self.play(Write(table.add_field(DecimalNumber(3.14))))
         
         self.play(table.adjust_lines())
 
