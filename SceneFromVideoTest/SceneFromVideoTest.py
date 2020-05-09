1f5980efcc8d6b01df6b2b4cfb016c141575f8c9
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-05-09 16:08:42 +0530

    SceneFromVideo Testing

---
+++ SceneFromVideoTest/SceneFromVideoTest.py
@@ -0,0 +1,6 @@
+from manimlib.imports import *
+
+class OverHand(SceneFromVideo):
+    
+    def construct(self):
+        SceneFromVideo.construct(self, "/Users/aathishs/Python/ManimEnv/GeneratedMedia/videos/TableExample/480p15/Tables.mp4")
