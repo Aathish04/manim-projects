a7f5e5df827c9a538a0806194c86426dbb791412
Aathish Sivasubrahmanian <aathishs@Aathishs-MacBook-Air.local>
2020-07-26 16:32:12 +0530

    Put Manim on a Graph

--- ManimAsFunctions.py
+++ ManimAsFunctions.py
@@ -4,27 +4,27 @@ class ManimAsFunctions(GraphScene):
     def MCurve(self):
         LeftMCurve = ParametricFunction(
             lambda t : RIGHT*(0.9*np.cos(t)-1.8) + UP*(3*np.sin(t)+3),
-            t_min=-PI/2,t_max=np.arcsin(-0.5/3)).set_color(GREEN)
+            t_min=-PI/2,t_max=np.arcsin(-0.5/3))
 
         MiddleMCurve = ParametricFunction(
             lambda t : RIGHT*(np.cos(t)) + UP*(np.sqrt(1.5)*np.sin(t)+3),
             t_min=-PI + np.arccos(np.sqrt(1.25/1.5)),
-            t_max=-np.arccos(np.sqrt(1.25/1.5))).set_color(GREEN)
+            t_max=-np.arccos(np.sqrt(1.25/1.5)))
 
         RightMCurve = ParametricFunction(
             lambda t : RIGHT*(0.9*np.cos(t)+1.8) + UP*(3*np.sin(t)+3),
-            t_min=np.arcsin(-2.125/3),t_max=-PI+np.arcsin(0.5/3)).set_color(GRAY)
-        return VGroup(LeftMCurve,MiddleMCurve,RightMCurve)
+            t_min=np.arcsin(-2.125/3),t_max=-PI+np.arcsin(0.5/3))
+        return VGroup(LeftMCurve,MiddleMCurve,RightMCurve).set_color(GREEN)
 
     def aCurve(self):
         aCircle=ParametricFunction(
             lambda t :RIGHT*(0.875*np.cos(t)+3.325)+UP*(0.875*np.sin(t)+0.875),
-            t_min=-PI,t_max=PI).set_color(BLUE)
+            t_min=-PI,t_max=PI)
         aTail= ParametricFunction(
             lambda t: RIGHT*(0.9*np.cos(t)+4.86) + UP*(3*np.sin(t)+3),
             t_min=-PI+np.arcsin(1.3/3),
-            t_max=-PI/2+np.arcsin(1.3/3)).set_color(GREEN)
-        return VGroup(aCircle,aTail)
+            t_max=-PI/2+np.arcsin(1.3/3))
+        return VGroup(aCircle,aTail).set_color(BLUE)
 
     def nCurve(self):
         nline=ParametricFunction(
@@ -36,14 +36,14 @@ class ManimAsFunctions(GraphScene):
             lambda t: RIGHT*(0.75*np.cos(t)+(np.arcsin(1.3/3)+4.86)+0.75) + UP*(1.8*np.sin(t)),
             t_min=0,
             t_max=PI
-        ).set_color(GREEN)
+        ).set_color(ORANGE)
         return VGroup(nline,nCurve)
 
     def iLine(self):
         iline = ParametricFunction(
             lambda t: UP*t + RIGHT*(np.arcsin(1.3/3)+4.86+1.8),
             t_min=0,t_max=1.75
-        ).set_color(BLUE)
+        ).set_color(RED)
         idot = Dot().move_to(iline).shift(UP*1.755).set_color(RED)
         return VGroup(iline,idot)
 
@@ -54,12 +54,41 @@ class ManimAsFunctions(GraphScene):
         return mcurve
 
     def construct(self):
-        ManimCurves = VGroup(
-            self.MCurve(),
-            self.aCurve(),
-            self.nCurve(),
-            self.iLine(),
-            self.mCurve()
-            )
-        ManimCurves.set_x(0)
-        self.play(Write(ManimCurves))
+        ManimCurves = VDict()
+        ManimCurves["MCurve"]=self.MCurve()
+        ManimCurves["aCurve"]=self.aCurve()
+        ManimCurves["nCurve"]=self.nCurve()
+        ManimCurves["iLine"]=self.iLine()
+        ManimCurves["mCurve"]=self.mCurve()
+        ManimCurves.set_x(0).set_stroke(width=10)
+
+        x_axis = NumberLine(
+            x_min=0,
+            x_max=ManimCurves.get_width(),
+            unit_size=1,
+            numbers_with_elongated_ticks=range(0,13,3),
+            color=WHITE,
+            include_tip=True,
+            add_start=True,
+            add_end=True,
+        ).move_to(ORIGIN)
+
+        y_axis = NumberLine(
+            x_min=0,
+            x_max=ManimCurves.get_height(),
+            unit_size=1,
+            leftmost_tick=self.y_bottom_tick,
+            numbers_with_elongated_ticks=range(0,4),
+            color=WHITE,
+            include_tip=True,
+            add_start=True,
+            add_end=True
+        )
+        y_axis.shift(ManimCurves.get_corner(LEFT+DOWN) - y_axis.number_to_point(0))
+        y_axis.rotate(PI / 2, about_point=y_axis.number_to_point(0))
+
+        axes= VGroup(x_axis,y_axis)
+        axesandgraph = VGroup(axes,ManimCurves).shift(DOWN)
+        self.play(Write(axes))
+        for m in ManimCurves:
+            self.play(Write(m),run_time=0.5)
